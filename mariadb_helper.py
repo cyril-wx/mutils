64f0835253d3104f3256a7a32c0a092df77f774b
cyril <848873227@qq.com>
2018-12-08 07:34:54 +0000

    Update mariadb_helper.py

--- mariadb_helper.py
+++ mariadb_helper.py
@@ -17,15 +17,17 @@ class MariadbHelper(object):
                 'charset':'utf8', }     ## 如果不设置字符集则会导致输出中文乱码
         # 初始化db连接
         def __init__(self, config={}):
-                self.db = pymysql.connect(**self.config)
+                if config:
+                        self.db = pymysql.connect(**config)
+                else:
+                        self.db = pymysql.connect(**self.config)
                 print("Connecting mysql.")
-
         # 析构函数
         def __del__(self):
                 '''
                 del析构函数，并不是在del a对象的时候就会调用该析构函数
                 只有当该对象的引用计数为0时才会调用析构函数，回收资源
-                析构函数被python的垃圾回收器销毁的时候调用。当某一个对象没有被引用时，垃圾回收器自动回收资源，调用析构函数
+                析构函数被python的垃圾回收器销毁的时候调用。当某一个对象没有被引用时，垃圾回收器自动回收资源，调用>析构函数
                  '''
                 self.db.close() # 销毁对象时同时关闭数据库连接
                 print("Disconnecting mysql.")
@@ -42,13 +44,15 @@ class MariadbHelper(object):
                         a = a[0:-1]
                 else:
                         a = "*"
+
                 if not table_name:  # 如果table_name为空, 则直接中断返回
                         return -1
-                sql_cmd = "select " + a + " from " + table_name
-                #print("sql_select -> sql_cmd = ", sql_cmd)
-                exec_sql_cmd(sql_cmd)
 
+                sql_cmd = "select " + a + " from " + table_name
+                print("sql_select -> sql_cmd = ", sql_cmd)
+                exec_sql_cmd(sql_cmd)
                 return data
+
 
         def exec_sql_cmd(self, sql_cmd):
                 cursor = self.db.cursor()  #创建游标使用的cursor方法
@@ -65,14 +69,47 @@ class MariadbHelper(object):
                         self.db.rollback()  # 如果发生错误则回滚
                 finally:
                         cursor.close()
+        def submit_exec(self):
+                self.db.commit()
 
 if __name__ == '__main__':
-    
-        mh = MariadbHelper()
-        data = mh.exec_sql_cmd("""
-                select * from punch_info;
-""")
+        from datetime import datetime
+        import time
 
-        print("TestResult: ", data)
-        pass
+        config = {
+                'host':'127.0.0.1',
+                'port':3306,    ## MySQL默认端口
+                'user':'root',  ## mysql默认用户名
+                'password':'zxc12345',
+                'db':'EISP_PERS',       ## 数据库
+                'charset':'utf8', }     ## 如果不设置字符集则会导致输出中文乱码
+
+        # * : 元组参数， ** : 字典参数
+        mh = MariadbHelper(config)
+
+        import csv_rw
+        csv_file = "./eisp_pers_punch_info.csv"
+        data =  csv_rw.readCSVFile(csv_file)
+        print(data)
+
+        time_begin = datetime.now()
+        # 写入50w+ 条数据
+        for line in data:
+
+              MAX_NUM = 10000
+              for num in range(0, MAX_NUM):
+                print("NUM= ", num)
+                if num >= 10000: break;
+
+                sql_cmd = """insert into `punch_info` (`job_num`,`name`,`punch_loc`,`punch_type`,`punch_date`,`puch_time`)
+                        VALUES('{0}','{1}','{2}','{3}','{4}','{5}');
+                """.format(line[1],line[2],line[3],line[4],line[5],line[6])
+         #       print("sql_cmd= ",sql_cmd)
+
+                data = mh.exec_sql_cmd(sql_cmd)
+                print("TestResult: ", data)
+
+        time_end = datetime.now()
+        print("Total Time(s): ", (time_end-time_begin).seconds)
         
+        #树莓派写入50w+ 数据花费 2346s
