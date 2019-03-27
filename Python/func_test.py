243bf7a5fc34af40e9c439e000893ce686a13748
cyril <848873227@qq.com>
2019-03-27 14:45:55 +0000

    Add func_test.py

---
+++ Python/func_test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding:UTF-8 -*-
+import sys
+
+def func_1(a, b):
+	print("func_1: a=%s, b=%s" %(a,b))
+
+def func_2(a):
+	print("func_2: a=%s" %a)
+
+if __name__ == "__main__":
+	module = sys.modules[__name__]
+	# getattr() 函数用于返回一个对象属性值。
+	# sys.argv 是获取运行python文件的时候命令行参数,且以list形式存储参数 
+	# sys.argv[0] 代表当前module的名字
+	func = getattr(module, sys.argv[1])
+	args = None
+	if len(sys.argv) > 1:
+		args = sys.argv[2:]
+		
+	func(*args)
