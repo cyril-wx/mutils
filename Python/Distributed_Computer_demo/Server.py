519382aa9ebcc9a3994e780ff85f59036af9eb46
cyril <848873227@qq.com>
2019-07-04 16:12:12 +0000

    更新Python/Distributed_Computer_demo/Server.py

--- Python/Distributed_Computer_demo/Server.py
+++ Python/Distributed_Computer_demo/Server.py
@@ -1,14 +1,9 @@
-#task_master.py
+# task_manager.py
 # -*- coding:utf-8 -*-
-
-#多进程分布式例子
-#服务器端
-
-# master服务端原理：通过managers模块把Queue通过网络暴露出去，其他机器的进程就可以访问Queue了
-# 服务进程负责启动Queue，把Queue注册到网络上，然后往Queue里面写入任务，代码如下:
-
-import random, time, queue
+import random, queue
 from multiprocessing.managers import BaseManager
+import numpy as np
+import time
 
 # 发送任务的队列
 task_queue = queue.Queue()
@@ -18,14 +13,14 @@ result_queue = queue.Queue()
 
 # 使用标准函数来代替lambda函数，避免python2.7中，pickle无法序列化lambda的问题
 def get_task_queue():
-    global task_queue
-    return task_queue
+	global task_queue
+	return task_queue
 
 
 # 使用标准函数来代替lambda函数，避免python2.7中，pickle无法序列化lambda的问题
 def get_result_queue():
-    global task_queue
-    return task_queue
+	global task_queue
+	return task_queue
 
 
 def startManager(host, port, authkey):
@@ -39,23 +34,54 @@ def startManager(host, port, authkey):
     return manager
 
 
-def put_queue(manager):
-    # 通过网络访问queueu
-    task = manager.get_task_queue()
-    while 1:
-        n = random.randint(0, 1000)
-        print ('Put task %d' % n)
-        task.put(n)
-        time.sleep(0.5)
+def put_queue(manager, objs):
+	# 通过网络访问queueu
+	task = manager.get_task_queue()
+	for obj in objs:
+		try:
+			print("Put obj:{}".format(obj))
+			task.put(obj)
+			time.sleep(1)
+		except queue.Full:
+			print("put_queue task full.exit ")
+			break
+
+
+
+def get_result(worker):
+	result = worker.get_result_queue()
+	while 1:
+		try:
+			n = result.get(timeout=1)
+			print("Result get {}".format(n))
+			time.sleep(1)
+		except queue.Empty:
+			print("get_result result empty...retring")
+			continue
+		else:
+			pass
+
+
 
 
 if __name__ == "__main__":
+
     host = '127.0.0.1'
     port = 5000
     authkey = b'abc'
     # 启动manager服务器
     manager = startManager(host, port, authkey)
+
+    # 数据
+    data = np.arange(0,20)
+
     # 给task队列添加数据
-    put_queue(manager)
+    put_queue(manager, data)
+    #get_queue(manager)
+
+    get_result(manager)
+
     # 关闭服务器
     manager.shutdown
+
+
